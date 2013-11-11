890b5fa12de3878fb2681edfff0c13bb95c2cf71
Johan Kllstrm <johka546@student.liu.se>
2013-11-11 19:37:11 +0100

    LAB1: datainsammling

---
+++ lab1/plottar.m
@@ -0,0 +1,72 @@
+%% a 
+load a_01 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+%%
+load a_02 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+%%
+load o_01 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+%%
+load o_02 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+
+%%
+load vissel_01 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+%%
+load vissel_02 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+%%
+load fox_01 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
+
+%%
+load fox_02 % Load data (a_sound <-> name of file)
+figure(1)
+plot(y(1,:),y(2,:)) % Signal amplitude vs. time
+figure(2)
+plot(abs(fft(y(2,:))))
+
+soundsc(y(2,:),8000) % Listen to the sound
