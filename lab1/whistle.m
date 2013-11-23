59044c20272a80aae8f9323d7b9ac5f5415b5958
Johan <Johan Kallstrom>
2013-11-23 16:44:40 +0100

    Wistle: smfix

--- lab1/whistle.m
+++ lab1/whistle.m
@@ -27,11 +27,12 @@ legend('Orginal','Trimmad')
 pdf_print('whistle_fft.pdf')
 
 %energi-inneh�ll
-U_tot=sum(vissel_02_iddatae.y.^2)
-U_tot_f=sum(abs(fft(vissel_02_iddatae.y)).^2)/length(vissel_02_iddatae.y)
+U_tot=Ts*sum(vissel_02_iddatae.y.^2)
+U_tot_f=Ts*sum(abs(fft(vissel_02_iddatae.y)).^2)/length(vissel_02_iddatae.y)
 
 %% bp-filter
 [b,a]=butter(5,[1300*2/fs 1700*2/fs]);
+freqz(b,a);
 vissel_02_bp = filtfilt(b,a,vissel_02_iddatae.y);
 vissel_02_bp_iddata = iddata(vissel_02_bp,[],Ts)
 
@@ -47,11 +48,47 @@ legend('Ofiltrerad','Filtrerad')
 pdf_print('whistle_filtrerad.pdf')
 
 %energi-innehåll
-U_bp=sum(vissel_02_bp_iddata.y.^2)
-U_bp_f=sum(abs(fft(vissel_02_bp_iddata.y)).^2)/length(vissel_02_bp_iddata.y) 
+U_bp=Ts*sum(vissel_02_bp_iddata.y.^2)
+U_bp_f=Ts*sum(abs(fft(vissel_02_bp_iddata.y)).^2)/length(vissel_02_bp_iddata.y) 
+
+
+%% bp-filter 2
+F_lower=1535
+F_upper=1545
+[b,a]=butter(5,[F_lower*2/fs F_upper*2/fs]);
+figure(20)
+freqz(b,a);
+pdf_print('whistle_filter_response.pdf')
+
+vissel_02_bp = filtfilt(b,a,vissel_02_iddatae.y);
+vissel_02_bp_iddata = iddata(vissel_02_bp,[],Ts)
+
+% plotta frekvensspektra för filtrerad och ofiltrerad
+figure(21)
+plot(chgFreqUnit(fft(vissel_02_iddatae),'Hz'))
+hold on
+plot(chgFreqUnit(fft(vissel_02_bp_iddata),'Hz'),'r')
+hold off
+title('Whistle frekvensspektra ')
+xlabel('Frekvens[Hz]')
+legend('Ofiltrerad','Filtrerad')
+pdf_print('whistle_filtrerad_1535_1545.pdf')
+
+%energi-innehåll
+U_bp=Ts*sum(vissel_02_bp_iddata.y.^2)
+U_bp_f=Ts*sum(abs(fft(vissel_02_bp_iddata.y)).^2)/length(vissel_02_bp_iddata.y) 
+
+sig=fft(vissel_02_iddatae.y);
+%sig=sig.y;
+
+figure(100)
+plot(abs(sig))
+
+U_bp_f_strict=Ts*sum(abs(sig(floor(F_lower/(fs/length(vissel_02_iddatae.y))):floor(F_upper/(fs/length(vissel_02_iddatae.y))))).^2)/length(vissel_02_iddatae.y)*2 % times 2 because we are using only half the spectra to calc the enegy
 
 
 harmonic_distorsion=1-U_bp/U_tot
+harmonic_distorsion_strict=1-U_bp_f_strict/U_tot
 %% AR
 figure(13)
 arx_rm_trans_bp = arx(vissel_02_bp_iddata,[2], arxOptions);
@@ -65,8 +102,8 @@ legend('Filtrerad','Ofiltrerad')
 pdf_print('whistle_pz_map.pdf')
 
 %avstånd till enhetscirkeln
-dist_rm_trans_bp=1-abs(roots(arx_rm_trans_bp.a))
-dist_org=1-abs(roots(arx_org.a))
+dist_roots_bp=1-abs(roots(arx_rm_trans_bp.a))
+dist_roots_org=1-abs(roots(arx_org.a))
 
 e = randn(1,length(vissel_02_bp_iddata.y));
 yhat = filter(1,arx_rm_trans_bp.a,e);
